9c3c7b46bdf6670cb1a422d804740cf498604f9d
gronblack <30817266+gronblack@users.noreply.github.com>
2020-03-02 19:17:20 +0700

    Настроен sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path');
 const fs = require("fs");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const PATHS = {
-    src: path.join(__dirname, './src'),
-    dist: path.join(__dirname, './dist')
+    src: path.resolve(__dirname, './src'),
+    dist: path.resolve(__dirname, './dist')
 };
 
 // Pages const for HtmlWebpackPlugin
@@ -13,16 +14,27 @@ const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.p
 const PAGES_UI = fs.readdirSync(PAGES_DIR + '/UI/').filter(fileName => fileName.endsWith('.pug'));
 
 module.exports = {
-    entry: PATHS.src,
+    entry: {
+        app: PATHS.src + '/index.js'
+    },
     output: {
         path: PATHS.dist,
-        filename: '[name].bundle.js'
+        filename: '[name].js'
     },
     module: {
         rules: [
             {
                 test: /\.pug$/,
                 loader: 'pug-loader'
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    { loader: 'css-loader', options: { sourceMap: true } },
+                    { loader: 'sass-loader', options: { sourceMap: true } },
+                ]
             }
         ]
     },
@@ -34,6 +46,12 @@ module.exports = {
         ...PAGES_UI.map(page => new HtmlWebpackPlugin({
             template: PAGES_DIR + 'UI/' + page,
             filename: 'UI/' + page.replace(/\.pug/,".html")
-        }))
-    ]
+        })),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        })
+    ],
+    resolve: {
+        extensions: ['.js', '.scss', '.pug']
+    }
 };
