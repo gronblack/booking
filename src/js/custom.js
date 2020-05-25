3056f68e06b910efc3dca61ed38d2260dc6d79ef
gronblack <30817266+gronblack@users.noreply.github.com>
2020-05-25 17:18:31 +0700

    Product card

--- src/js/custom.js
+++ src/js/custom.js
@@ -407,5 +407,38 @@ $(document).ready(function () {
     }
   });
   // ----------- like-button end ------------------
+  
+  
+  // ----------- product-card begin ------------------
+  $('.product-card').each(function () {
+    var card = $(this);
+    var list = card.find('.product-card__list');
+
+    var controlsWrapper = card.find('.product-card__controls');
+    var controls = controlsWrapper.find('.product-card__control');
+    var controlFirst = controls.first();
+    var controlLast = controls.last();
+
+    // list sliding on controls click
+    controls.on('click', function () {
+      list.find('.product-card__item_current').removeClass('product-card__item_current');
+      list.find(`[data-control=${$(this).attr('for')}]`).addClass('product-card__item_current');
+    });
+
+    // list sliding on arrows click
+    card.find('.product-card__arrow-left, .product-card__arrow-right').on('click', function () {
+      var currentItem = list.find('.product-card__item_current');
+      var currentControl = controlsWrapper.find(`[for=${currentItem.data('control')}]`);
+      if ($(this).data('way') === 'left') {
+        var newControl = currentControl.prevAll('.product-card__control').first();
+        if (!newControl.length) newControl = controlLast;
+      } else {
+        var newControl = currentControl.nextAll('.product-card__control').first();
+        if (!newControl.length) newControl = controlFirst;
+      }
+      newControl.trigger('click');
+    });
+  });
+  // ----------- product-card end ------------------
 
 });
