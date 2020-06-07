2a49954eb3804963dcd52804c00ed2a51c096831
gronblack <30817266+gronblack@users.noreply.github.com>
2020-06-07 19:50:48 +0700

    Header adaptive

--- src/js/custom.js
+++ src/js/custom.js
@@ -474,5 +474,9 @@ $(document).ready(function () {
       .on('mouseleave', function () {
         $(this).find('.header__nav-submenu').removeClass('header__nav-submenu_visible');
       });
+
+  $('.header__nav-toggle').on('click', function () {
+    $(this).closest('.header__nav').toggleClass('header__nav_expand');
+  });
   // ----------- header end ----------------------
 });
